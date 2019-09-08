f40ce74ed8f378656f6731bdcec8af04638e7e75
tp4348 <tp4348@student.uni-lj.si>
2019-09-08 17:21:13 +0200

    error handling without KeyboardInterrupt

--- SpeechProcessing--Python.py
+++ SpeechProcessing--Python.py
@@ -1,13 +1,19 @@
 import speech_recognition as SR
 import time
+import sys
 
-Rec = SR.Recognizer()
-Mic = SR.Microphone()
+def SpeechRec() :
+    Rec = SR.Recognizer()
+    Mic = SR.Microphone()
 
-try :
-    with Mic as source :
-        Rec.adjust_for_ambient_noise(source, duration=0.5)
-        audio = Rec.listen(source)
-    print(Rec.recognize_google(audio))
-except KeyboardInterrupt :
-    pass
+    while True :
+        try :
+            with Mic as source :
+                Rec.adjust_for_ambient_noise(source, duration=0.5)
+                audio = Rec.listen(source)
+                print(Rec.recognize_google(audio))
+
+        except :
+            print("Error")
+    
+    
